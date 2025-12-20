2f46d870c5c6464ff6d427d31f08f0e8d6e7a88a
jovanihrnndz <jovanihrnndz@outlook.com>
2025-12-20 00:49:47 -0800

    Initial Commit

---
+++ IBBLB/IBBLBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBBLBApp.swift
+//  IBBLB
+//
+//  Created by jovani hernandez on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct IBBLBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
